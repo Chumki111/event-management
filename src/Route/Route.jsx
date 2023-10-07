b3c9f841c56f09544d71217f6c4758446be9232a
Chumki khatun <chumkikhatun370@gamil.com>
2023-10-07 08:19:20 +0600

    intial project set up with react react router tailwind css daisyUi and inter google font

---
+++ src/Route/Route.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+ 
+ const myCreatedRoute = createBrowserRouter([
+    {
+        path : '/',
+        element : <div>hello</div>
+    }
+ ])
+ 
+ export default myCreatedRoute;
